c601981b03196fd276665b68f454ba06c724c6f2
Lin Bing-Jin <bingjin.lin@gmail.com>
2019-07-15 10:55:49 +0800

    initial import

---
+++ src/entrylevel/EntryLevelApp.java
@@ -0,0 +1,23 @@
+package entrylevel;
+
+
+public class EntryLevelApp {
+
+  // EntryLevelApp 程式進入點
+  public static void main(String pArgs[]) {
+    EntryLevelApp entryLevelApp = new EntryLevelApp();
+    entryLevelApp.runLevel1();
+  }
+
+  /*
+    使用 Level1 所提供的 public 函式
+    1. 產生一個正整數陣列
+    2. 找出 1. 的陣列中的最大正整數，將此正整數輸出到終端機
+    3. 找出 1. 的陣列中的最小正整數，將此正整數輸出到終端機
+
+    NOTE: 使用 new 運算子產生一個 Level1 的實例(instance) 並使用所提供的 public 函式。
+   */
+  private void runLevel1() {
+
+  }
+}
---
+++ src/entrylevel/Level1.java
@@ -0,0 +1,50 @@
+package entrylevel;
+
+/*
+    1. 基本型別認識與使用
+    2. 基本變數宣告
+    3. 基本陣列宣告與使用
+    4. flow-control statement
+
+    可以使用 System.out.println() 函式將變數內容輸出到終端機上，
+    System.out 函式說明參考 https://docs.oracle.com/javase/8/docs/api/java/lang/System.html#out
+ */
+public class Level1 {
+
+  /*
+   * 變數宣告與陣列使用
+   */
+  public int[] array() {
+    int len = 10;
+    // 宣告一陣列長度為 len，並將陣列利用 for 迴圈將陣列的元素分別給予由1到10的數字。
+    // 以下寫程式碼
+
+    // 將陣列內容輸出到終端機上
+    // 以下寫程式碼
+
+
+    // 將陣列中的每個元素，分別乘上一個隨機的正整數，
+    // 隨機的數字可以使用 java.util.Random 類別。
+    // 參考 https://docs.oracle.com/javase/8/docs/api/java/util/Random.html#nextInt--
+    // 以下寫程式碼
+
+
+    // 傳回值改成前面宣告的陣列變數
+    return null;
+  }
+
+  // 找出輸入的陣列中的最大正整數。
+  // 傳回陣列中最大數前，將陣列內容輸出到終端機上
+  public int max(int pNumbers[]) {
+
+    return 0;
+  }
+
+  // 找出輸入的陣列中的最小正整數。
+  // 傳回陣列中最小正整數前，將陣列內容輸出到終端機上
+  public int min(int pNumbers[]) {
+
+    return 0;
+  }
+
+}
